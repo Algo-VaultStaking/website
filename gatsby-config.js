e49ab77bb4a1b05b8668fab8d328fd95e11b5a2c
algo-vaultstaking <vaultstaking@gmail.com>
2023-11-22 07:18:01 -0500

    packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `localhost:8080`
   },
   plugins: ["gatsby-plugin-sass", {
     resolve: 'gatsby-plugin-google-analytics',
